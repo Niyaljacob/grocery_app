3eb66f5e6e18aea68b812f6829f760d42fc58931
niyal jacob <niyaljacob76@gmail.com>
2024-06-08 17:33:15 +0530

    add the bloc pages

---
+++ lib/features/home/ui/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[100],
+        title: Center(child: Text('Grocery App',style: TextStyle(fontWeight: FontWeight.bold),)),
+      ),
+    );
+  }
+}
